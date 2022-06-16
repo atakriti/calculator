f7d897b66d49968d92fac5cbda495fe0a0115596
atakriti <anwart256@gmail.com>
2022-06-16 15:38:35 +0200

    mobile version done

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,20 @@
 let display = document.querySelector(".display");
 let buttons = document.querySelectorAll("button");
+let pop = document.querySelector(".pop")
+let pop1 = document.querySelector(".pop1")
+
 // ======== Background Color ===============
 let colorBgInput = document.querySelector("#c-btn");
+let colorBgInputMo = document.querySelector("#c-btn-mo");
 // ============== Font Color =============
 let colorFontInput = document.querySelector("#c-font");
+let colorFontInputMo = document.querySelector("#c-font-mo");
 // ========== forms ==========
-let form = document.querySelectorAll("form");
+let form = document.querySelectorAll(".form");
+let formMo = document.querySelectorAll(".form1");
 // ========= custom color btn =========
 let customColor = document.querySelector(".custom-color")
-let pop = document.querySelector(".pop")
+let customColorMo = document.querySelector(".mo")
 buttons.forEach((p) => {
   p.addEventListener("click", (e) => {
     if (e.target.innerText == "C") {
@@ -40,9 +46,33 @@ form.forEach((p) => {
       buttons.forEach((b) => {
         b.style.color = colorFontInput.value;
       });
-    }
+      } 
+
+     
+
   });
 });
+
+
+
+formMo.forEach(p => {
+    console.log(p)
+    p.addEventListener("submit", (e) => {
+        e.preventDefault()
+        if (p.matches("#c-btn-form-mo")) {
+            buttons.forEach(b => {
+                console.log(b)
+                b.style.backgroundColor = colorBgInputMo.value
+            })
+        } else if (p.matches("#c-font-form-mo")) {
+            buttons.forEach(b => {
+                b.style.color = colorFontInputMo.value
+            })
+        }
+    })
+})
+
+
 
 // ========= custom color ========
 // pop.style.width = "0"
@@ -50,7 +80,7 @@ pop.style.transform = "translate(30%)"
 pop.style.opacity = "0"
 customColor.addEventListener("click", () => {
     if (pop.style.transform === "translate(30%)") {
-       setTimeout(() => ( pop.style.transform = "translate(0%)",pop.style.opacity = "1"),100)
+        setTimeout(() => (pop.style.transform = "translate(0%)", pop.style.opacity = "1"), 100)
         // pop.style.opacity = "1"
         // pop.classList.add("popUp")
         pop.style.animation = "popUp 2s"
@@ -61,5 +91,24 @@ customColor.addEventListener("click", () => {
 
         // pop.style.opacity = "0"
         // pop.classList.add("popOut")
+    }
+})
+
+
+// ================= mobile ====================
+
+pop1.style.transform = "scale(0)"
+pop1.style.opacity = "0"
+customColorMo.addEventListener("click", () => {
+    if (pop1.style.transform === "scale(0)") {
+        setTimeout(() => (pop1.style.transform = "scale(1)", pop1.style.opacity = "1"), 100)
+        pop1.style.animation = "popScaleUp 2s"
+    } else {
+       setTimeout(() => ( pop1.style.transform = "scale(0)",pop1.style.opacity = "0"),1000)
+    //    pop1.style.animation = "popOut 2s"
+    pop1.style.animation = "popScaleDown 2s"
+        
+
+  
     }
 })
